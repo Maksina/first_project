49e98989f2c73dfb43e5cdf196f3f624c81b20b4
Maksina <m.schurenkov@gmail.com>
2018-11-24 14:22:56 +0400

    Sum++

--- main.c
+++ main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-
+void sum (const int* ar1, const int* ar2, int* res, int count);
 int min(const int* array, int count);
 int main()
 { int count,lv_min;
-  int array[10];
-  count = 10;
+  int array[9];
+  count = 9;
   array[0] = 432;
   array[1] = 3213;
-    array[2] = 241;
+  array[2] = 241;
   array[3] = 11324;
-    array[4] = 100;
+  array[4] = 100;
   array[5] = 200;
-    array[6] = 412;
+  array[6] = 412;
   array[7] = 4312;
-    array[8] = 42;
-  array[9] = 312;
+  array[8] = 42;
+
   lv_min = min(array, count);
   printf("%d\n", lv_min);
   
@@ -29,5 +29,7 @@ int min( const int* array, int count) {
   }   
   }
   return min;
+}
+void sum (const int* ar1, const int* ar2, int* res, int count) {
   
 }
